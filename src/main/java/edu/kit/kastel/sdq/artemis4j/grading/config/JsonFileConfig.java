703b833dacf100826012b70775c8c430822807af
CDaut <github@cdaut.de>
2023-07-26 22:53:02 +0200

    Getter for a config file of a JSONConfig (#7)
    * getter for a config file of a JSONConfig
    
    * added method to get allowed exercises from a JSONConfig without
    specifying an exercise
    
    * Spotless
    
    ---------
    
    Co-authored-by: CDaut <git@cdaut.de>
    Co-authored-by: Dominik Fuchß <develop@fuchss.org>

--- src/main/java/edu/kit/kastel/sdq/artemis4j/grading/config/JsonFileConfig.java
+++ src/main/java/edu/kit/kastel/sdq/artemis4j/grading/config/JsonFileConfig.java
@@ -8,6 +8,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Implementation of {@link GradingConfig} using a json file.
@@ -35,4 +37,24 @@ public class JsonFileConfig implements GradingConfig {
 	private void parse() throws IOException, IllegalStateException {
 		this.exerciseConfig = oom.readValue(this.configFile, ExerciseConfig.class);
 	}
+
+	public File getConfigFile() {
+		return configFile;
+	}
+
+	/**
+	 * Get a list of all exercises that can be graded by this config
+	 *
+	 * @return A copy of the deserialized List of allowed exercise IDs
+	 * @throws IOException if parsing the config file fails
+	 */
+	public List<Integer> getAllowedExercises() throws IOException {
+		if (this.exerciseConfig == null) {
+			this.parse();
+		}
+
+		return new ArrayList<>(this.exerciseConfig.getAllowedExercises());
+
+	}
+
 }
