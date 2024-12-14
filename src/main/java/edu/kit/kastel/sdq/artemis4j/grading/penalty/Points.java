640765fa455280b73a15ac27bf59631d24931435
Luro02 <24826124+Luro02@users.noreply.github.com>
2024-12-14 12:08:21 +0100

    address review

--- src/main/java/edu/kit/kastel/sdq/artemis4j/grading/penalty/Points.java
+++ src/main/java/edu/kit/kastel/sdq/artemis4j/grading/penalty/Points.java
@@ -1,10 +1,4 @@
 /* Licensed under EPL-2.0 2024. */
 package edu.kit.kastel.sdq.artemis4j.grading.penalty;
 
-public record Points(double score, boolean capped) {
-    public Points {
-        if (score == -0.0) {
-            score = 0.0;
-        }
-    }
-}
+public record Points(double score, boolean capped) {}
