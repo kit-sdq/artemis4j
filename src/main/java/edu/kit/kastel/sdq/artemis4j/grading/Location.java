86d95c91e583e03c92b0593c1807363bee56a2ce
Luro02 <24826124+Luro02@users.noreply.github.com>
2025-01-29 12:10:38 +0100

    implement `Comparable` for `Location`

--- src/main/java/edu/kit/kastel/sdq/artemis4j/grading/Location.java
+++ src/main/java/edu/kit/kastel/sdq/artemis4j/grading/Location.java
@@ -1,7 +1,10 @@
 /* Licensed under EPL-2.0 2025. */
 package edu.kit.kastel.sdq.artemis4j.grading;
 
-public record Location(String filePath, LineColumn start, LineColumn end) {
+import java.util.Comparator;
+import java.util.Objects;
+
+public record Location(String filePath, LineColumn start, LineColumn end) implements Comparable<Location> {
     public Location {
         if (start.compareTo(end) > 0) {
             throw new IllegalArgumentException("start %s must be before end %s".formatted(start, end));
@@ -14,5 +17,32 @@ public record Location(String filePath, LineColumn start, LineColumn end) {
 
     public Location(String filePath, int startLine, int endLine) {
         this(filePath, LineColumn.entireLine(startLine), LineColumn.entireLine(endLine));
+    }
+
+    @Override
+    public int compareTo(Location other) {
+        return Comparator.comparing(Location::filePath)
+                .thenComparing(Location::start)
+                .thenComparing(Location::end)
+                .compare(this, other);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Location location)) {
+            return false;
+        }
+
+        return Objects.equals(this.end(), location.end())
+                && Objects.equals(this.filePath(), location.filePath())
+                && Objects.equals(this.start(), location.start());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(this.filePath());
+        result = 31 * result + Objects.hashCode(this.start());
+        result = 31 * result + Objects.hashCode(this.end());
+        return result;
     }
 }
